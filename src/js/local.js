76b7536d7735c2055b9f58228b97dc3763932507
Dmitry Sviridenko <letehaha@gmail.com>
2017-09-01 14:38:48 +0300

    added contact page & minor fixes

--- src/js/local.js
+++ src/js/local.js
@@ -2,3 +2,28 @@ $(document).ready(function () {
     var body = $('body');
 
 });
+
+(function(){
+
+  var form = document.getElementById('contact__form'),
+      returnForm = document.getElementById('return-contact-form');
+
+  form.onsubmit = function(e){
+    e.preventDefault();
+    
+    var _this = this;
+
+    _this.style.display = 'none';
+    _this.nextElementSibling.style.display = 'block';
+    
+    // ajax ..
+    return false;
+  };
+
+  returnForm.onclick = function(e){
+    this.parentNode.style.display = 'none';
+    this.parentNode.previousElementSibling.style.display = 'block';
+  }
+
+
+})();
